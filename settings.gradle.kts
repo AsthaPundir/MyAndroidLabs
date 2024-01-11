10600507b489f84552f78fa97670397c4c7b2e6a
asthapundir <pund0006@algonquinlive.com>
2024-01-11 00:58:12 -0500

    Initial Commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "astha's android labs"
+include(":app")
